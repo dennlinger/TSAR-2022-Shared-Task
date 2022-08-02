90953cf4fb6e39642c6b6f2dfbd189acc531ecb2
Dennis Aumiller <dennis.aumiller@gmx.de>
2022-08-02 11:19:29 +0200

    Removing explicit device call.

--- load_large_model.py
+++ load_large_model.py
@@ -7,7 +7,6 @@ if __name__ == '__main__':
     device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
 
     model = GPTJForCausalLM.from_pretrained("EleutherAI/gpt-j-6B", revision="float16", low_cpu_mem_usage=True)
-    model.to(device)
     tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B")
     pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, device=device)
 
