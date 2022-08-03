1e971b128ebb4bbfb9218b4fb5971d86a1967226
Dennis Aumiller <dennis.aumiller@gmail.com>
2022-08-03 10:31:01 +0200

    Fixing issues with fp16 and device.

--- load_large_model.py
+++ load_large_model.py
@@ -4,9 +4,10 @@ from transformers import GPTJForCausalLM, AutoTokenizer, pipeline
 
 if __name__ == '__main__':
 
-    device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+    device = 1 if torch.cuda.is_available() else "cpu"
 
-    model = GPTJForCausalLM.from_pretrained("EleutherAI/gpt-j-6B", revision="float16", low_cpu_mem_usage=True)
+    model = GPTJForCausalLM.from_pretrained("EleutherAI/gpt-j-6B", revision="float16", torch_dtype=torch.float16, low_cpu_mem_usage=True)
+    model.eval()
     tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B")
     pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, device=device)
 
