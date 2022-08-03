107f34fb99221b2306c0a7794e61f22db314e267
Dennis Aumiller <dennis.aumiller@gmx.de>
2022-08-03 10:41:48 +0200

    Adding code skeleton for 20B model handling.

--- load_large_model.py
+++ load_large_model.py
@@ -1,12 +1,13 @@
 import torch
-from transformers import GPTJForCausalLM, AutoTokenizer, pipeline
+from transformers import GPTJForCausalLM, AutoTokenizer, pipeline, AutoConfig, AutoModelForCausalLM
+from accelerate import load_checkpoint_and_dispatch, init_empty_weights
 
 
-if __name__ == '__main__':
-
+def load_6b_model():
     device = 1 if torch.cuda.is_available() else "cpu"
 
-    model = GPTJForCausalLM.from_pretrained("EleutherAI/gpt-j-6B", revision="float16", torch_dtype=torch.float16, low_cpu_mem_usage=True)
+    model = GPTJForCausalLM.from_pretrained("EleutherAI/gpt-j-6B", revision="float16",
+                                            torch_dtype=torch.float16, low_cpu_mem_usage=True)
     model.eval()
     tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B")
     pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, device=device)
@@ -14,3 +15,15 @@ if __name__ == '__main__':
     prompt = "Give a synonym for the following word: compulsory\nSynonym:"
 
     print(pipe(prompt, do_sample=True, temperature=0.6, max_length=20))
+
+
+if __name__ == '__main__':
+    checkpoint = "EleutherAI/gpt-neox-20b"
+    config = AutoConfig.from_pretrained(checkpoint)
+
+    with init_empty_weights():
+        model = AutoModelForCausalLM.from_config(config)
+
+    model = load_checkpoint_and_dispatch(
+        model, "gpt-neox-20b", device_map="auto", max_memory={"0": "20GIB"}
+    )
